7e69f90405b330a8f74ece835938485e599a200a
Nithanth Sawkar <85395596+NithanthSawkar@users.noreply.github.com>
2023-02-13 22:54:00 +0530

    Create pes2ug20cs230.cpp

---
+++ main/pes2ug20cs230.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << " Changes successful";
+  cout <<"Hello";
+  return 0;
+}
